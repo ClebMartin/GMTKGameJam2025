470ff496c10438e026172ed6f35baea846024b12
Cole Spears <colespears@MacBook-Pro-10.local>
2025-08-01 23:47:37 -0600

    Next Droppable queue

--- gmtk-game-jam-2025/scripts/player.gd
+++ gmtk-game-jam-2025/scripts/player.gd
@@ -25,6 +25,10 @@ signal dropped_droppable
 var _current_droppable
 var _current_droppable_id = 0
 
+# For "Queue" in Game
+var _next_droppable_id
+signal next_droppable_id_signal
+
 var _tick = 0
 var _waiting_to_spawn = false
 
@@ -35,12 +39,15 @@ func _pick_random_droppable():
 	return _rng.randi_range(0, 3)
 	
 func _spawn_new_droppable():
-	_current_droppable_id = _pick_random_droppable()
+	_current_droppable_id = _next_droppable_id
+	_next_droppable_id = _pick_random_droppable()
 	_current_droppable = _droppable_img_list[_current_droppable_id]
 	_current_droppable.show()
+	next_droppable_id_signal.emit(_next_droppable_id)
 
 func _ready():
 	_set_up_list()
+	_next_droppable_id = 0
 	_spawn_new_droppable()
 
 func _physics_process(delta):
