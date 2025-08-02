465709f9d8e9a330f56492eebc70fc15c0cc95fe
Cole Spears <colespears@MacBook-Pro-10.local>
2025-08-01 20:59:37 -0600

    angular velocity and physics updates

--- gmtk-game-jam-2025/scripts/droppable_base.gd
+++ gmtk-game-jam-2025/scripts/droppable_base.gd
@@ -4,6 +4,7 @@ class_name DroppableBase
 signal droppable_collided
 
 @export var droppable_id = SUIKA.DROPPABLES
+@onready var _rng = RandomNumberGenerator.new()
 
 var disabled = false
 
@@ -11,6 +12,11 @@ func _init():
 	contact_monitor = true
 	#maybe the bug is here?
 	max_contacts_reported = 5
+	
+	var a_velocity = randi_range(PI/2, PI)
+	self.angular_velocity = a_velocity
+	if randi_range(0, 1) == 0:
+		self.angular_velocity *= -1
 
 func _physics_process(delta: float):
 	var contacts = get_colliding_bodies()
@@ -26,12 +32,13 @@ func _physics_process(delta: float):
 				# maybe a dropped flower touches more than one of a collision?
 				if not body.disabled:
 					if droppable_id == body.droppable_id:
-						disabled = true
-						body.disabled = true
-						body.queue_free()
-						# Create next evolution at self.position
-						# Which means, if the sizes of the droppables
-						# are unbalanced, game might look funky
-						droppable_collided.emit(position, droppable_id)
-						queue_free()
+						if not disabled: # have to check if original is disabled, else, other droppables would change too
+							disabled = true
+							body.disabled = true
+							body.queue_free()
+							# Create next evolution at self.position
+							# Which means, if the sizes of the droppables
+							# are unbalanced, game might look funky
+							droppable_collided.emit(position, droppable_id)
+							queue_free()
 			pass
